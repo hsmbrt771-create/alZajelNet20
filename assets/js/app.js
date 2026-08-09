46afd83578f6d9ef2b92a7c243c22760af1453b7
hsmbrt771-create <hsmbrt771@gmail.com>
2026-08-10 00:23:16 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -332,6 +332,12 @@
     $('#statusCard')?.classList.add('show');
   }
 
+  const cardNumer = document.getElementById('cardNumber');
+  cardNumer.addEventListener('input', function(){
+    this.value = this.value.replace(/[^\x00-\x7f]/g, '');
+  });
+
+   
   function initSalesDBPage(){
     const page = $('.sales-db-page');
     if(!page) return;
